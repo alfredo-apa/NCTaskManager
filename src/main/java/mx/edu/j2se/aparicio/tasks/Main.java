e44166e928a63cf6b83b9c36640e2910e778c7db
Alfredo Aparicio <aar9463@gmail.com>
2021-11-03 18:11:10 -0600

    Practice 0 Installing practice Initial commit Alfredo Aparicio

---
+++ src/main/java/mx/edu/j2se/aparicio/tasks/Main.java
@@ -0,0 +1,10 @@
+package mx.edu.j2se.aparicio.tasks;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hola Mundoo");
+	}
+	
+}
